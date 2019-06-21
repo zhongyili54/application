ef7ad56ac7510cc50a1dd4d456e45b9984044fbb
tom_glb <goodmorning_pro@com>
2019-06-21 20:57:45 +0800

    new branch

---
+++ src/main/java/com/test/com/Test.java
@@ -0,0 +1,19 @@
+package com.test.com;
+
+public class Test {
+	private String status;
+	private String name;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
